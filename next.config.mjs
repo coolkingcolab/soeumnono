52cc176d4fc16864d94f1437dd2271b99b7a93c8
ADK <jhp0106@gmail.com>
2025-08-17 18:44:02 +0900

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
