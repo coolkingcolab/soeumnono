4f9a6e5ad988c0006bf6536263fd2e64772d1bd7
ADK <jhp0106@gmail.com>
2025-08-19 10:51:56 +0900

    Update report.ts

--- types/report.ts
+++ types/report.ts
@@ -9,4 +9,6 @@ export interface Report {
   score: number; // 1점에서 5점까지의 소음 점수
   noiseTypes: string[]; // 소음 종류 배열 (예: ["발걸음", "가구 끌기"])
   createdAt: FieldValue | Timestamp; // 평가 등록 시간
+  lat?: number; // 위도
+  lng?: number; // 경도
 }
